63c6d3250bad25e2f9c4a148290131d5582894c8
Paolo Fisco <paolo.fisco@gmail.com>
2016-07-10 22:34:10 +0200

    Create app.js

---
+++ desio/app.js
@@ -0,0 +1,29 @@
+// Code goes here
+
+var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
+
+function MyController($scope, $http) {
+
+  $scope.currentPage = 1;
+  $scope.pageSize = 20;
+  $scope.items = [];
+
+ $http.get("https://raw.githubusercontent.com/infocitta/dataset/master/desio/livello2.json")
+    .then(function(response) {
+        $scope.items = response.data;
+    });
+	
+	
+  $scope.pageChangeHandler = function(num) {
+      
+  };
+}
+
+function OtherController($scope) {
+  $scope.pageChangeHandler = function(num) {
+  
+  };
+}
+
+myApp.controller('MyController', MyController);
+myApp.controller('OtherController', OtherController);
