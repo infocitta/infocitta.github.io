2a0b2e6dbfe086e2a30a312fc8d366f28c2535de
Paolo Fisco <paolo.fisco@gmail.com>
2016-07-10 19:33:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,53 +2,26 @@
 
 var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
 
-function MyController($scope) {
+function MyController($scope, $http) {
 
   $scope.currentPage = 1;
-  $scope.pageSize = 10;
-  $scope.meals = [];
+  $scope.pageSize = 20;
+  $scope.items = [];
 
-  var dishes = [
-    'noodles',
-    'sausage',
-    'beans on toast',
-    'cheeseburger',
-    'battered mars bar',
-    'crisp butty',
-    'yorkshire pudding',
-    'wiener schnitzel',
-    'sauerkraut mit ei',
-    'salad',
-    'onion soup',
-    'bak choi',
-    'avacado maki'
-  ];
-  var sides = [
-    'with chips',
-    'a la king',
-    'drizzled with cheese sauce',
-    'with a side salad',
-    'on toast',
-    'with ketchup',
-    'on a bed of cabbage',
-    'wrapped in streaky bacon',
-    'on a stick with cheese',
-    'in pitta bread'
-  ];
-  for (var i = 1; i <= 100; i++) {
-    var dish = dishes[Math.floor(Math.random() * dishes.length)];
-    var side = sides[Math.floor(Math.random() * sides.length)];
-    $scope.meals.push('meal ' + i + ': ' + dish + ' ' + side);
-  }
-  
+ $http.get("https://raw.githubusercontent.com/infocitta/dataset/master/livello1.json")
+    .then(function(response) {
+        $scope.items = response.data;
+    });
+	
+	
   $scope.pageChangeHandler = function(num) {
-      console.log('meals page changed to ' + num);
+      
   };
 }
 
 function OtherController($scope) {
   $scope.pageChangeHandler = function(num) {
-    console.log('going to page ' + num);
+  
   };
 }
 
