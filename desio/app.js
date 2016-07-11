a8568ea755cd1f1cad38013c11f7f1a49d5dbb71
Paolo Fisco <paolo.fisco@gmail.com>
2016-07-11 12:29:48 +0200

    Update app.js

--- desio/app.js
+++ desio/app.js
@@ -8,7 +8,7 @@ function MyController($scope, $http) {
   $scope.pageSize = 20;
   $scope.items = [];
 
- $http.get("livello2.json")
+ $http.get("level2.json")
     .then(function(response) {
         $scope.items = response.data;
     });
