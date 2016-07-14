026628776e9a9046a5976dbc7b3b5661cdf3b00a
Paolo Fisco <paolo.fisco@gmail.com>
2016-07-14 22:51:42 +0200

    deploy sito

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Home InfoCittà
-
+# master
---
+++ lissone/mercato/app.js
@@ -0,0 +1,30 @@
+// Code goes here
+
+var myApp = angular.module('MyApp', ['angularUtils.directives.dirPagination','ngMap']);
+
+function MyController($scope, $http) {
+
+
+  $scope.currentPage = 1;
+  $scope.pageSize = 20;
+  $scope.items = [];
+
+ $http.get("entities.json")
+    .then(function(response) {
+        $scope.items = response.data;
+    });
+	
+	
+  $scope.pageChangeHandler = function(num) {
+      
+  };
+}
+
+function OtherController($scope) {
+  $scope.pageChangeHandler = function(num) {
+  
+  };
+}
+
+myApp.controller('MyController', MyController);
+myApp.controller('OtherController', OtherController);
